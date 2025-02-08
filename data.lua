bd980188ba73cde34dd4b56b634019df467ec54b
rjdunlap <rjdunlap@outlook.com>
2025-02-08 15:29:13 -0800

    0.0.7 update

--- data.lua
+++ data.lua
@@ -1,8 +1,11 @@
 --data.lua
+local utils = require("__any-planet-start__.utils")
 local asteroid_util = require("__space-age__.prototypes.planet.asteroid-spawn-definitions")
 local planet_catalogue_vulcanus = require("__space-age__.prototypes.planet.procession-catalogue-vulcanus")
-
-require("vicrox")
+local resource_autoplace = require("resource-autoplace")
+local sounds = require("__base__.prototypes.entity.sounds")
+local simulations = require("__space-age__.prototypes.factoriopedia-simulations")
+local item_sounds = require("__base__.prototypes.item_sounds")
 
 --START MAP GEN
 function MapGen_Vicrox()
@@ -142,7 +145,6 @@ function MapGen_Vicrox()
         ["big-sand-rock"] = {},
         ["huge-rock"] = {},
         ["big-rock"] = {},
-        
         }
     }
      
@@ -453,6 +455,11 @@ data:extend {{
             type = "unlock-recipe",
             recipe = "vicrox-scrap-recycling",
         },
+        {
+          type = "unlock-recipe",
+          recipe = "ice-melting",
+      },
+        
     },
     prerequisites = {
         "space-science-pack",
@@ -470,5 +477,91 @@ data:extend {{
     order = "ea[vicrox]",
 }}
 
+
+local merge = require("lib").merge
+
+-- Custom Scrap
+
+data:extend({
+	merge(data.raw.resource["scrap"], {
+		name = "vicrox-scrap",
+		icon_size = 64,
+		order = "w-a[vicrox-scrap]",
+		minable = merge(data.raw.resource["scrap"].minable, {
+			mining_time = 0.33,
+			result = "vicrox-scrap",
+		}),
+		map_color = { 0.18, 0.22, 0.2 },
+		map_grid = true,
+		factoriopedia_simulation = "nil",
+	}),})
+
+data:extend{{
+    type = "item",
+    name = "vicrox-scrap",
+    icon = "__space-age__/graphics/icons/scrap.png",
+    pictures =
+    {
+      { size = 64, filename = "__space-age__/graphics/icons/scrap.png",   scale = 0.5, mipmap_count = 4 },
+      { size = 64, filename = "__space-age__/graphics/icons/scrap-1.png", scale = 0.5, mipmap_count = 4 },
+      { size = 64, filename = "__space-age__/graphics/icons/scrap-2.png", scale = 0.5, mipmap_count = 4 },
+      { size = 64, filename = "__space-age__/graphics/icons/scrap-3.png", scale = 0.5, mipmap_count = 4 },
+      { size = 64, filename = "__space-age__/graphics/icons/scrap-4.png", scale = 0.5, mipmap_count = 4 },
+      { size = 64, filename = "__space-age__/graphics/icons/scrap-5.png", scale = 0.5, mipmap_count = 4 }
+    },
+    subgroup = "fulgora-processes",
+    order = "a[vicrox-scrap]-a[vicrox-scrap]",
+    inventory_move_sound = item_sounds.resource_inventory_move,
+    pick_sound = item_sounds.resource_inventory_pickup,
+    drop_sound = item_sounds.resource_inventory_move,
+    stack_size = 50,
+    default_import_location = "vicrox",
+    weight = 2*kg
+  }
+}
+
+data:extend{
+{
+    type = "recipe",
+    name = "vicrox-scrap-recycling",
+    icons = {
+      {
+        icon = "__quality__/graphics/icons/recycling.png"
+      },
+      {
+        icon = "__space-age__/graphics/icons/scrap.png",
+        scale = 0.4
+      },
+      {
+        icon = "__quality__/graphics/icons/recycling-top.png"
+      }
+    },
+    category = "recycling-or-hand-crafting",
+    subgroup = "fulgora-processes",
+    order = "a[trash]-a[trash-recycling]",
+    enabled = false,
+    auto_recycle = false,
+    energy_required = 0.4,
+    ingredients = {{type = "item", name = "vicrox-scrap", amount = 1}},
+    results =
+    {
+      {type = "item", name = "ice",                    amount = 1, probability = 0.25, show_details_in_recipe_tooltip = false},
+      {type = "item", name = "iron-ore",               amount = 1, probability = 0.20, show_details_in_recipe_tooltip = false},
+      {type = "item", name = "scrap",                  amount = 1, probability = 0.10, show_details_in_recipe_tooltip = false},
+      {type = "item", name = "copper-ore",             amount = 1, probability = 0.10, show_details_in_recipe_tooltip = false},
+      {type = "item", name = "coal",                   amount = 1, probability = 0.05, show_details_in_recipe_tooltip = false},
+      {type = "item", name = "stone",                  amount = 1, probability = 0.03, show_details_in_recipe_tooltip = false},
+      {type = "item", name = "stone-brick",            amount = 1, probability = 0.03, show_details_in_recipe_tooltip = false},
+      {type = "item", name = "wooden-chest",           amount = 1, probability = 0.025, show_details_in_recipe_tooltip = false},
+      {type = "item", name = "uranium-ore",            amount = 1, probability = 0.01, show_details_in_recipe_tooltip = false},
+      {type = "item", name = "raw-fish",               amount = 1, probability = 0.01, show_details_in_recipe_tooltip = false},
+      {type = "item", name = "sulfur",                 amount = 1, probability = 0.01, show_details_in_recipe_tooltip = false},
+
+    }
+  }
+}
+
+utils.set_prerequisites("recycling", nil)
+utils.remove_tech("uranium-processing", true, true)
 
 APS.add_planet{name = "vicrox", filename = "__planet-vicrox__/vicrox.lua", technology = "planet-discovery-vicrox"}
